89cbcd70ebacb90eb83bc3c22a5c94f804a6b09d
qkrwjdduf159 <pde159@naver.com>
2023-12-25 23:36:04 +0900

    :lock: 2023-12-25

--- 프로그래머스/SQL/SUM, MAX, MIN.sql
+++ 프로그래머스/SQL/SUM, MAX, MIN.sql
@@ -23,3 +23,5 @@ SELECT COUNT(DISTINCT NAME) FROM ANIMAL_INS;
 -- 동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회하는 SQL문을 작성해주세요. 이때 고양이를 개보다 먼저 조회해주세요.
 SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) FROM ANIMAL_INS GROUP BY ANIMAL_TYPE HAVING ANIMAL_TYPE='Cat' OR ANIMAL_TYPE='Dog' ORDER BY ANIMAL_TYPE ASC;
 
+-- PRODUCT 테이블에서 판매 중인 상품 중 가장 높은 판매가를 출력하는 SQL문을 작성해주세요. 이때 컬럼명은 MAX_PRICE로 지정해주세요
+SELECT MAX(PRICE) AS MAX_PRICE FROM PRODUCT
