69d21004e70d82de6160292e6f12f4f03afb7ed0
qkrwjdduf159 <pde159@naver.com>
2023-12-26 14:43:00 +0900

    :lock: 2023-12-26

--- 프로그래머스/SQL/IS NULL.sql
+++ 프로그래머스/SQL/IS NULL.sql
@@ -10,3 +10,8 @@ SELECT ANIMAL_ID FROM ANIMAL_INS WHERE NAME IS NOT NULL;
 -- 입양 게시판에 동물 정보를 게시하려 합니다. 동물의 생물 종, 이름, 성별 및 중성화 여부를 아이디 순으로 조회하는 SQL문을 작성해주세요. 이때 프로그래밍을 모르는 사람들은 NULL이라는 기호를 모르기 때문에, 이름이 없는 동물의 이름은 "No name"으로 표시해 주세요.
 SELECT ANIMAL_TYPE, IFNULL(NAME,'No name'), SEX_UPON_INTAKE FROM ANIMAL_INS 
 
+-- FOOD_WAREHOUSE 테이블에서 경기도에 위치한 창고의 ID, 이름, 주소, 냉동시설 여부를 조회하는 SQL문을 작성해주세요. 이때 냉동시설 여부가 NULL인 경우, 'N'으로 출력시켜 주시고 결과는 창고 ID를 기준으로 오름차순 정렬해주세요.
+SELECT WAREHOUSE_ID, WAREHOUSE_NAME, ADDRESS, IFNULL(FREEZER_YN,'N') AS FREEZER_YN FROM FOOD_WAREHOUSE WHERE ADDRESS LIKE '경기%' ORDER BY WAREHOUSE_ID ASC
+
+-- USER_INFO 테이블에서 나이 정보가 없는 회원이 몇 명인지 출력하는 SQL문을 작성해주세요. 이때 컬럼명은 USERS로 지정해주세요.
+SELECT COUNT(*) AS USERS FROM USER_INFO WHERE AGE IS NULL
