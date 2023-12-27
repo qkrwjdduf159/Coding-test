95d1a807f305afe6887250afee086165d1da57bc
qkrwjdduf159 <pde159@naver.com>
2023-12-27 16:09:06 +0900

    :memo: 2023-12-27

--- 프로그래머스/SQL/GROUP BY.sql
+++ 프로그래머스/SQL/GROUP BY.sql
@@ -29,3 +29,26 @@ on A.num=hour(B.DATETIME)
 GROUP BY A.num
 ORDER BY A.num
 LIMIT 24;
+
+
+-- REST_INFO 테이블에서 음식종류별로 즐겨찾기수가 가장 많은 식당의 음식 종류, ID, 식당 이름, 즐겨찾기수를 조회하는 SQL문을 작성해주세요. 이때 결과는 음식 종류를 기준으로 내림차순 정렬해주세요.
+-- 처음엔 SELECT FOOD_TYPE, REST_ID, REST_NAME, MAX(FAVORITES) FROM REST_INFO 형식으로 작성을 했으나 문제는 REST_ID와 REST_NAME이 랜덤으로 나와서 정답이 나올 수가 없었다.
+-- 담부터도 이렇게 지정을 해줘야 문제 없이 쿼리문을 작성할 수 있을 것 같다.
+SELECT A.FOOD_TYPE, A.REST_ID, A.REST_NAME, A.FAVORITES 
+FROM REST_INFO AS A 
+INNER JOIN 
+(SELECT FOOD_TYPE, MAX(FAVORITES) AS FAVORITES FROM REST_INFO GROUP BY FOOD_TYPE) AS B
+ON A.FOOD_TYPE=B.FOOD_TYPE AND A.FAVORITES=B.FAVORITES
+ORDER BY FOOD_TYPE DESC
+
+-- USED_GOODS_BOARD와 USED_GOODS_USER 테이블에서 완료된 중고 거래의 총금액이 70만 원 이상인 사람의 회원 ID, 닉네임, 총거래금액을 조회하는 SQL문을 작성해주세요. 결과는 총거래금액을 기준으로 오름차순 정렬해주세요.
+-- A와 B의 INNERJOIN을 진행했을 때 ON을 통해서 두 테이블의 조건을 맞춰주는 것이 중요하다.
+SELECT B.USER_ID, B.NICKNAME, A.TOTAL_SALES 
+FROM (SELECT WRITER_ID, SUM(PRICE) AS TOTAL_SALES FROM USED_GOODS_BOARD WHERE STATUS='DONE' GROUP BY WRITER_ID) AS A 
+INNER JOIN (SELECT USER_ID, NICKNAME FROM USED_GOODS_USER) AS B 
+ON A.WRITER_ID=B.USER_ID
+WHERE A.TOTAL_SALES>=700000 ORDER BY TOTAL_SALES
+
+-- PRODUCT 테이블에서 만원 단위의 가격대 별로 상품 개수를 출력하는 SQL 문을 작성해주세요.
+-- 이때 컬럼명은 각각 컬럼명은 PRICE_GROUP, PRODUCTS로 지정해주시고 가격대 정보는 각 구간의 최소금액(10,000원 이상 ~ 20,000 미만인 구간인 경우 10,000)으로 표시해주세요. 결과는 가격대를 기준으로 오름차순 정렬해주세요.
+SELECT FLOOR(PRICE/10000)*10000 AS PRICE_GROUP, COUNT(*) PRODUCT FROM PRODUCT GROUP BY PRICE_GROUP ORDER BY PRICE_GROUP
