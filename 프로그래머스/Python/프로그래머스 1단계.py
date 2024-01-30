5e1bcaf3aea9b18c827617b349730f78172ef483
qkrwjdduf159 <pde159@naver.com>
2024-01-30 22:57:07 +0900

    :memo: 2024-01-30

--- 프로그래머스/Python/프로그래머스 1단계.py
+++ 프로그래머스/Python/프로그래머스 1단계.py
@@ -72,3 +72,49 @@ def solution(strings, n):
     answer = sorted(strings, key=lambda x:x[n])
 
     return answer
+
+# 정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+def solution(numbers):
+    number_list = []
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i!=j:
+                number_list.append(numbers[i] + numbers[j])
+                
+    answer = sorted(list(set(number_list)))
+    return answer
+
+# 네오는 평소 프로도가 비상금을 숨겨놓는 장소를 알려줄 비밀지도를 손에 넣었다. 그런데 이 비밀지도는 숫자로 암호화되어 있어 위치를 확인하기 위해서는 암호를 해독해야 한다. 다행히 지도 암호를 해독할 방법을 적어놓은 메모도 함께 발견했다.
+
+# 지도는 한 변의 길이가 n인 정사각형 배열 형태로, 각 칸은 "공백"(" ") 또는 "벽"("#") 두 종류로 이루어져 있다.
+# 전체 지도는 두 장의 지도를 겹쳐서 얻을 수 있다. 각각 "지도 1"과 "지도 2"라고 하자. 지도 1 또는 지도 2 중 어느 하나라도 벽인 부분은 전체 지도에서도 벽이다. 지도 1과 지도 2에서 모두 공백인 부분은 전체 지도에서도 공백이다.
+# "지도 1"과 "지도 2"는 각각 정수 배열로 암호화되어 있다.
+# 암호화된 배열은 지도의 각 가로줄에서 벽 부분을 1, 공백 부분을 0으로 부호화했을 때 얻어지는 이진수에 해당하는 값의 배열이다.
+def solution(n, arr1, arr2):
+    ## arr1과 arr2를 2진법으로 변경
+    arr1_list = []
+    arr2_list = []
+    for i,j in zip(arr1,arr2):
+        a = bin(i)[2:]
+        b = bin(j)[2:]
+        
+        if len(a)<=n:
+            a = (n-len(a)) * '0' + a
+        if len(b)<=n:
+            b = (n-len(b)) * '0' + b
+        arr1_list.append(a)
+        arr2_list.append(b)
+        
+    ## arr1과 arr2를 통해 정답
+    answer = []
+    for num1, num2 in zip(arr1_list, arr2_list):
+        str_1 = ''
+        for num_1, num_2 in zip(num1, num2):
+            if (num_1 == '0') & (num_2=='0'):
+                str_1 = str_1 + ' '
+            else:
+                str_1 = str_1 + '#'
+                
+        answer.append(str_1)
+        
+    return answer
