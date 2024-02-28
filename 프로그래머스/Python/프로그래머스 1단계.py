028e6d63182cf48254fe51de5791c59e023ecd5e
qkrwjdduf159 <pde159@naver.com>
2024-02-28 22:11:10 +0900

    :memo: 2024-02-28

--- 프로그래머스/Python/프로그래머스 1단계.py
+++ 프로그래머스/Python/프로그래머스 1단계.py
@@ -265,3 +265,21 @@ def solution(nums):
         if b==1:
             answer +=1
     return answer
+
+
+
+## 1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수, solution을 만들어 보세요.
+## 소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
+## (1은 소수가 아닙니다.)
+
+def solution(n):
+    answer = 0
+    for i in range(2, n+1):
+        num = int(i**0.5)
+        sub_answer = []
+        for k in range(2,num+1):
+            if i%k==0:
+                break
+        else:
+            answer += 1
+    return answer
