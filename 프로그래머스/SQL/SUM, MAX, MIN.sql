89639abe1c01b97a9199359706bae8c2fa5a76c8
qkrwjdduf159 <pde159@naver.com>
2024-03-18 23:23:53 +0900

    :memo:

--- 프로그래머스/SQL/SUM, MAX, MIN.sql
+++ 프로그래머스/SQL/SUM, MAX, MIN.sql
@@ -28,3 +28,11 @@ SELECT MAX(PRICE) AS MAX_PRICE FROM PRODUCT
 
 -- ITEM_INFO 테이블에서 희귀도가 'LEGEND'인 아이템들의 가격의 총합을 구하는 SQL문을 작성해 주세요. 이때 컬럼명은 'TOTAL_PRICE'로 지정해 주세요.
 SELECT SUM(PRICE) AS TOTAL_PRICE FROM ITEM_INFO WHErE RARITY='LEGEND'
+
+-- 물고기 종류 별로 가장 큰 물고기의 ID, 물고기 이름, 길이를 출력하는 SQL 문을 작성해주세요. 물고기의 ID 컬럼명은 ID, 이름 컬럼명은 FISH_NAME, 길이 컬럼명은 LENGTH로 해주세요. 결과는 물고기의 ID에 대해 오름차순 정렬해주세요.
+-- 단, 물고기 종류별 가장 큰 물고기는 1마리만 있으며 10cm 이하의 물고기가 가장 큰 경우는 없습니다.
+SELECT A.ID, B.FISH_NAME, A.LENGTH 
+FROM (SELECT ID, FISH_TYPE, LENGTH FROM FISH_INFO WHERE (FISH_TYPE,LENGTH) IN (SELECT FISH_TYPE, MAX(LENGTH) AS LENGTH FROM FISH_INFO GROUP BY FISH_TYPE)) AS A 
+LEFT JOIN FISH_NAME_INFO AS B 
+ON A.FISH_TYPE=B.FISH_TYPE
+ORDER BY 1
