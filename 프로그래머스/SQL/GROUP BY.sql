d703d290a6827fc0559c77a1cf5cea3c34a99a04
qkrwjdduf159 <pde159@naver.com>
2024-03-18 17:00:25 +0900

    :memo: 2024-03-18

--- 프로그래머스/SQL/GROUP BY.sql
+++ 프로그래머스/SQL/GROUP BY.sql
@@ -157,3 +157,11 @@ FROM (SELECT EMP_NO, EMP_NAME, SAL FROM HR_EMPLOYEES) AS A
 LEFT JOIN (SELECT EMP_NO, AVG(SCORE) AS SCORE FROM HR_GRADE GROUP BY EMP_NO) AS B 
 ON A.EMP_NO=B.EMP_NO
 ORDER BY EMP_NO
+
+-- 월별 잡은 물고기의 수와 월을 출력하는 SQL문을 작성해주세요. 잡은 물고기 수 컬럼명은 FISH_COUNT, 월 컬럼명은 MONTH로 해주세요. 결과는 월을 기준으로 오름차순 정렬해주세요.
+-- 단, 월은 숫자형태 (1~12) 로 출력하며 9 이하의 숫자는 두 자리로 출력하지 않습니다. 잡은 물고기가 없는 월은 출력하지 않습니다.
+SELECT COUNT(*) AS FISH_COUNT, MONTH(TIME) AS MONTH 
+FROM FISH_INFO 
+GROUP BY MONTH(TIME) 
+ORDER BY 2
+
